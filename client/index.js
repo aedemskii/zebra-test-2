efd896653d263b9c424bf4b4965200132f5cf087
Alexander Edemskii <a_edemskii@EdemMBP.local>
2024-04-23 16:02:17 +0300

    nested structure with nodes explansion

--- client/index.js
+++ client/index.js
@@ -1,7 +1,25 @@
 import './styles.scss';
 
 const main = () => {
-  console.log('Hello World');
+
+  const menu = document.querySelector('.menu');
+  if (!menu) {
+    return;
+  }
+  menu.addEventListener('click', menuClickHandler);
+
+  function handleButtonClick(target) {
+    const targetContainer = target.closest('.menu-item__title');
+    targetContainer.nextElementSibling.classList.toggle('show');
+  };
+
+  function menuClickHandler(e) {
+    e.preventDefault();
+    const target = e.target;
+    if (target.classList.contains('menu-item__submenu-button')) {
+      handleButtonClick(target);
+    }
+  };
 };
 
 document.addEventListener('DOMContentLoaded', main);
